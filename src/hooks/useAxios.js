e4a52ba410df2558fedab93e5f7f4a20be92900b
000xzhou <xzhou92@gmail.com>
2024-05-26 17:19:10 -0400

    opkedeck useaxios

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,13 +2,20 @@ import { useState } from "react";
 import axios from "axios";
 import { v1 as uuid } from "uuid";
 
-const useFlip = (url) => {
+const useAxios = (url) => {
   const [state, setState] = useState([]);
-  const handleState = async () => {
-    const response = await axios.get(url);
+  //   const handleState = async (options = {}) => {
+  const handleState = async (endpoint) => {
+    let fullURL;
+    if (typeof endpoint === "string") {
+      fullURL = url + endpoint;
+    } else {
+      fullURL = url;
+    }
+    const response = await axios.get(fullURL);
     setState((cards) => [...cards, { ...response.data, id: uuid() }]);
   };
   return [state, handleState];
 };
 
-export default useFlip;
+export default useAxios;
